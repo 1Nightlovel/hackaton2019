1baf8d500c2788790773486a6cfe163c0c1b1ed4
Mario <maa.fernandezg@gmail.com>
2019-06-22 02:07:00 -0500

    xdddddd

--- parkingserver/settings.py
+++ parkingserver/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = '/hackaton2019/parking/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'parking/static')
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
