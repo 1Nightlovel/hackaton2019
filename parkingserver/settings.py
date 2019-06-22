8a51a58b5b49be0a2ed536c10e07139d7a14f59a
Mario <maa.fernandezg@gmail.com>
2019-06-22 01:55:37 -0500

    asdasdasda

--- parkingserver/settings.py
+++ parkingserver/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'parkingserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'parking/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
