84738f477136716504238b11f247aaf8569c1183
Mario <maa.fernandezg@gmail.com>
2019-06-21 20:37:24 -0500

    setting

--- parkingserver/settings.py
+++ parkingserver/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$vfv*19ut%sqepf0rd)#sxq!fa0fg8zx1a8x$s$xl+cf#%**kf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
